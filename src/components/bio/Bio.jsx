9e82cba3f8c209bdf0fadf05bea3bcb9927f4024
MBAsh0023 <mmboardley@gmail.com>
2022-04-23 11:54:36 -0700

    update

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -5,23 +5,37 @@ export default function Bio() {
     <div className="bio" id="bio">
       <h1>About Me</h1>
       <div className="container">
-        
-
         <p className="item">
           {" "}
-          The moment I produced my first "Hello World" on the console sparked
+          The moment I produced my first "Hello World" on the console sparked my
           interest and I have not looked back. Software Development has become
           an arena of challenge and creativity for me. Becoming a developer has
           introduced me to a passion that will continue to teach me for a
           lifetime. My skills to this point allow me to be proficent in HTML,
-          CSS, JavaScript, and React. Ten years of airline operation mananagment has equipped me with analytical skills, communication skills and discipline to be an asset to any team.{" "}
+          CSS, JavaScript, and React. Ten years of airline operation mananagment
+          has equipped me with analytical skills, communication skills and
+          discipline to be an asset to any team.{" "}
         </p>
-        
       </div>
       <div className="profiles">
-      <a href="assets/Marquel-Ashlock-Resume.pdf" download><img src="assets/icons8-download-resume-100.png" alt="Resume"/></a>
-        <a href="https://github.com/MBAsh0023"><img src="assets/github-100.png" alt="GitHub"/></a>
-        <a href="https://www.linkedin.com/in/mbashlock/"><img src="assets/linkedin-100.png" alt="LinkedIn"/></a>
+        <div className="iconItem">
+          <a href="assets/Marquel-Ashlock-Resume.pdf" download>
+            <img src="assets/icons8-download-resume-100.png" alt="Resume" />
+          </a>
+          <h5>Resume Download</h5>
+        </div>
+        <div className="iconItem">
+          <a href="https://github.com/MBAsh0023">
+            <img src="assets/github-100.png" alt="GitHub" />
+          </a>
+          <h5>GitHub Profile</h5>
+        </div>
+        <div className="iconItem">
+          <a href="https://www.linkedin.com/in/mbashlock/">
+            <img src="assets/linkedin-100.png" alt="LinkedIn" />
+          </a>
+          <h5>LinkedIn Profile</h5>
+        </div>
       </div>
     </div>
   );
